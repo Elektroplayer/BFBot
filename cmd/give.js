b544cf23dfdb116cfc41fb10be626bd13a7609d3
Elektroplayer <vitaly.hawanski@yandex.ru>
2020-10-25 17:22:27 +0300

    3.3.3.1
    Переписан give

--- cmd/give.js
+++ cmd/give.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 const XP      = require('../models/xp.js');
 
 module.exports = {
-    run: (bot,message,args,con)=> {try{
+    run: async (bot,message,args,con)=> {try{
 
         if(args[0] == "help") {
             return message.channel.send(con.defEmb.setTitle("Помощь по команде give").setDescription("Передать свой уровень другому человеку").setFooter(con.footer)
@@ -25,25 +25,31 @@ module.exports = {
         if(!/^[0-9]{1,}$/g.test(args[1])) return addlib.errors.falseArgs(message,'Нужны только цифры!');
         let num = Number(args[1]);
         if(num<=0) return addlib.errors.falseArgs(message,`Число может быть только положительным!`);
+
+        let ok = true;
         
-        XP.findOne({userID: outUser}, (err, level) => {
+        await XP.findOne({userID: outUser}, (err, level) => {
             if(err) console.log(err);
                     
             if(!level) {
-                return addlib.errors.castom(message,"Мало XP!");
+                return ok = !ok
             } else {
                 let xpAll = 0;
                 let i=0;
-                for(;i<=level.level;i++) {xpAll += (Math.floor(i/2)+1);}
+                for(;i<=level.level;i++) {xpAll += 400+(120*i*2.5);}
                 xpAll+=level.xp-num;
+
+                console.log(xpAll)
                 
-                if(xpAll<0) return addlib.errors.castom(message,"Мало XP!");
+                if(xpAll<0) return ok = !ok
 
                 i=0;
                 for(;;i++) {
-                    if((Math.floor(i/2)+1)>xpAll) break;
-                    else xpAll-=(Math.floor(i/2)+1);
+                    if(400+(120*i*2.5)>xpAll) break;
+                    else xpAll-=400+(120*i*2.5);
                 }
+
+                console.log(`${i-1}, ${xpAll}`)
 
                 level.level = i-1;
                 level.xp = xpAll;
@@ -52,6 +58,8 @@ module.exports = {
             }
         })
 
+        if(!ok) return addlib.errors.castom(message,"Мало XP!");
+
         XP.findOne({userID: inUser}, (err, level) => {
             if(err) console.log(err);
                     
@@ -59,38 +67,43 @@ module.exports = {
                 let xpAll = num;
                 let i=0;
                 for(;;i++) {
-                    if((Math.floor(i/2)+1)>xpAll) break;
-                    else xpAll-=(Math.floor(i/2)+1);
+                    console.log(i)
+                    if(400+(120*i*2.5)>xpAll) break;
+                    else xpAll-= 400+(120*i*2.5);
                 }
 
-                var newXP =  new XP({
-                    userID: message.author.id,
-                    level: i-1,
+                console.log(`${i-1}, ${xpAll}`)
+
+                var newXP = new XP({
+                    userID: inUser,
+                    level: i,
                     xp: xpAll
                 })
 
-                message.channel.send(new discord.MessageEmbed().setColor('00ff00').setTitle(`${num}XP был успешно переведён ${message.guild.cache.get(inUser)}!`).setFoote(con.footer))
-                //addlib.errors.success(message,`XP был успешно переведён ${message.guild.cache.get(inUser)}!`)
-    
                 newXP.save().catch(err => console.log(err))
+
+                message.channel.send(new discord.MessageEmbed().setColor('00ff00').setTitle(`Транзакция в ${num}XP была успешно завершена!`).setDescription(`Получатель: ${message.guild.members.cache.get(inUser)}`).setFooter(con.footer))
+                //addlib.errors.success(message,`XP был успешно переведён ${message.guild.cache.get(inUser)}!`)
             } else {
                 let xpAll = 0
                 let i=0;
-                for(;i<=level.level;i++) {xpAll += (Math.floor(i/2)+1);}
+                for(;i<=level.level;i++) {xpAll += 400+(120*i*2.5);}
                 xpAll+=level.xp+num
 
                 i=0
                 for(;;i++) {
-                    if((Math.floor(i/2)+1)>xpAll) break;
-                    else xpAll-=(Math.floor(i/2)+1);
+                    if(400+(120*i*2.5)>xpAll) break;
+                    else xpAll-=400+(120*i*2.5);
                 }
+
+                console.log(`${i-1}, ${xpAll}`)
 
                 level.level = i-1
                 level.xp = xpAll
 
-                message.channel.send(new discord.MessageEmbed().setColor('00ff00').setTitle(`Транзакция в ${num}XP была успешно завершена!`).setDescription(`Получатель: ${message.guild.members.cache.get(inUser)}`).setFooter(con.footer))
-    
                 level.save().catch(err => console.log(err))
+
+                message.channel.send(new discord.MessageEmbed().setColor('00ff00').setTitle(`Транзакция в ${num}XP была успешно завершена!`).setDescription(`Получатель: ${message.guild.members.cache.get(inUser)}`).setFooter(con.footer))
             }
         })
     }catch(err){console.log(err)}},
